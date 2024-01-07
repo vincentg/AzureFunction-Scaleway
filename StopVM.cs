8d4a7f54cfc3ac3a3682e0e526e93e827c21f204
Vincent Gerard <vincentpgerard@gmail.com>
2024-01-06 18:12:54 -0600

    Implement first API call

--- StopVM.cs
+++ StopVM.cs
@@ -1,4 +1,7 @@
 using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
@@ -8,16 +11,44 @@ namespace vgerard.scaleway.management
     public class StopVM
     {
         private readonly ILogger _logger;
+        private static HttpClient sharedClient = new()
+        {
+            BaseAddress = new Uri("https://api.scaleway.com"),
+        };
 
         public StopVM(ILoggerFactory loggerFactory)
         {
             _logger = loggerFactory.CreateLogger<StopVM>();
+            sharedClient.DefaultRequestHeaders.Add("X-Auth-Token",
+                                                System.Environment.GetEnvironmentVariable("SCW_API_KEY"));
+        }
+
+        private StringContent actionPOSTPayload(string action) {
+            using StringContent jsonContent = new(JsonSerializer.Serialize(new
+                {
+                    action = action
+                }),
+                Encoding.UTF8,
+                "application/json");
+            return jsonContent;
         }
 
         [Function("StopVM")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", 
+               Route="stop/{zone:regex(^[A-Z]{{2}}-[A-Z]{{3}}-\\d{{1}}$)}/{server_id:regex(^[\\dA-Z-]{{36}}$)}")] HttpRequestData req,
+        string zone, string server_id)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            _logger.LogInformation($"StopVM HTTP trigger for zone: {zone}, serverID: {server_id}");
+
+            using HttpResponseMessage possibleActions = await sharedClient.GetAsync($"/instance/v1/zones/{zone}/servers/{server_id}/action");
+            possibleActions.EnsureSuccessStatusCode();
+            String? actions = await possibleActions.Content.ReadAsStringAsync();
+            _logger.LogInformation($"{actions}");
+/*
+            using HttpResponseMessage standby_res = await sharedClient.PostAsync(
+                "/instance/v1/zones/{param_zone}/servers/{param_server_id}/action",
+                actionPOSTPayload("stop_in_place"));
+*/
 
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
